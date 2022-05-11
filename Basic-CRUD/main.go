61bbf9e28b911b76bde5675e3335fc1451639397
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-10 21:48:50 -0300

    router call to getusers and getuser

--- Basic-CRUD/main.go
+++ Basic-CRUD/main.go
@@ -13,6 +13,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", server.GetUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.GetUser).Methods(http.MethodGet)
 
 	fmt.Println("Listening on http://localhost:5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
